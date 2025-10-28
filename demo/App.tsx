2025f690a6dfb9d698e498e81b3b06fe70dc2166
Tsuyoshi HARA <chloe463+git@gmail.com>
2025-10-29 00:14:39 +0900

    feat(demo): Add minimum demo

--- demo/App.tsx
+++ demo/App.tsx
@@ -1,11 +1,26 @@
-import './App.css'
+import "./App.css";
 
-function App() {
+import { PanelistProvider, PanelRenderer, type PanelId } from "../src";
+
+export default function App() {
   return (
-    <>
-      <h1>Panelist demo</h1>
-    </>
-  )
+    <div className="App">
+      <PanelistProvider
+        columnCount={6}
+        gap={8}
+        panelCoordinates={[
+          { id: 1, x: 0, y: 0, w: 2, h: 2 },
+          { id: 2, x: 2, y: 0, w: 2, h: 2 },
+          { id: 3, x: 4, y: 0, w: 2, h: 1 },
+          { id: 4, x: 0, y: 2, w: 1, h: 1 },
+        ]}
+      >
+        <PanelRenderer itemRenderer={PanelContent} />
+      </PanelistProvider>
+    </div>
+  );
 }
 
-export default App
+function PanelContent(id: PanelId) {
+  return <div className="panel-content">Panel Content {id}</div>;
+}
