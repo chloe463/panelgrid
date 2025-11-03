4ad81d8702feb7c7b72615f0c47410fb93cf150d
Tsuyoshi HARA <chloe463+git@gmail.com>
2025-11-03 17:03:33 +0900

    style: Configure husky and lint-staged

--- eslint.config.js
+++ eslint.config.js
@@ -18,6 +18,7 @@ export default defineConfig([
     rules: {
       "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
       "react-refresh/only-export-components": "off",
+      "no-console": "error",
     },
     languageOptions: {
       ecmaVersion: 2020,
