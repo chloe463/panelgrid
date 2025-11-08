85af6b5e951b4fbb12828be35bd9e2affeaabbae
Tsuyoshi HARA <chloe463+git@gmail.com>
2025-11-08 23:29:36 +0900

    feat(demo): Delete outdated lines

--- demo/App.tsx
+++ demo/App.tsx
@@ -1,6 +1,5 @@
 import "./App.css";
 
-// import { PanelistProvider, PanelRenderer, type PanelId } from "../src";
 import type { PanelId } from "../src/PanelistProvider";
 import { Panelist } from "../src/Panelist";
 import "../src/styles.css";
@@ -15,20 +14,10 @@ export default function App() {
           { id: 3, x: 4, y: 0, w: 2, h: 1 },
           { id: 4, x: 0, y: 2, w: 1, h: 1 },
         ]}
-        itemRenderer={PanelContent}
-      />
-      {/* <PanelistProvider
         columnCount={6}
         gap={8}
-        panelCoordinates={[
-          { id: 1, x: 0, y: 0, w: 2, h: 2 },
-          { id: 2, x: 2, y: 0, w: 2, h: 2 },
-          { id: 3, x: 4, y: 0, w: 2, h: 1 },
-          { id: 4, x: 0, y: 2, w: 1, h: 1 },
-        ]}
-      >
-        <PanelRenderer itemRenderer={PanelContent} />
-      </PanelistProvider> */}
+        itemRenderer={PanelContent}
+      />
     </div>
   );
 }
