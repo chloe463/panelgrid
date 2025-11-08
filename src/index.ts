aac9968596433911fd7067c91dda60a0eec2608b
Tsuyoshi HARA <chloe463+git@gmail.com>
2025-11-08 23:42:24 +0900

    chore: Remove some old components and hooks

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,3 @@ export { Panelist } from "./Panelist";
 export { usePanelist } from "./usePanelist";
 export type { PanelCoordinate } from "./usePanelist";
 export type { PanelId } from "./types";
-export { useGridConfig, useGridConfigControls, useDragState, useDragStateControls } from "./contexts";
